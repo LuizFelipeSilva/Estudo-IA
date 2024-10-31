59b5bc32935750c626f3f63b3afc1431de5b5fc1
LuizLacerdaDev <luiz.lacerda@allcomtelecom.com>
2024-10-31 11:23:36 -0300

    terminado modulo de EDA

--- teste_portifolio/sobre-mim.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const menuVideo = document.querySelector(".menu-video img");
-    const navbar = document.querySelector("header.navbar");
-
-    menuVideo.addEventListener("click", function() {
-        navbar.classList.toggle("expanded");
-    });
-});
